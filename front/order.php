75dc80c76a74c340102b1added4bd38ef3960c41
mackliu <mackliu@wda.gov.tw>
2023-02-10 09:31:37 +0800

    使用ajax 來取得上映中的電影列表，並顯示在電影選單中

--- front/order.php
+++ front/order.php
@@ -1,1 +1,40 @@
-訂票
+<h3 class="ct">線上訂票</h3>
+
+<table style="width:50%;margin:auto">
+    <tr>
+        <td>電影:</td>
+        <td>
+            <select name="" id="movie"></select>
+        </td>
+    </tr>
+    <tr>
+        <td>日期:</td>
+        <td>
+            <select name="" id=""></select>
+        </td>
+    </tr>
+    <tr>
+        <td>場次:</td>
+        <td>
+            <select name="" id=""></select>
+        </td>
+    </tr>
+</table>
+<div class="ct">
+<button>確定</button>
+<button>重置</button>
+</div>
+<script>
+
+
+getMovies();
+
+function getMovies(){
+    $.get("./api/get_movies.php",(movies)=>{
+        $("#movie").html(movies)
+
+    })
+}
+
+
+</script>
