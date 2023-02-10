20e1ff06b19b385e2991279bc903ef4a3a240d4a
mackliu <mackliu@wda.gov.tw>
2023-02-10 16:03:40 +0800

    使用js來判斷使用者的劃位狀況,並紀錄劃位內容

--- api/get_booking.php
+++ api/get_booking.php
@@ -1,7 +1,7 @@
 <?php  include_once "base.php";
 
 
-$bookings=[3,6,18,19,11,14];
+$bookings=[];
 
 ?>
 
@@ -52,7 +52,7 @@ $bookings=[3,6,18,19,11,14];
         <?php
             for($i=0;$i<20;$i++){
                 if(in_array($i,$bookings)){
-                    
+
                     echo "<div class='booking-seat'>";
                 }else{
                     echo "<div class='null-seat'>";
@@ -62,7 +62,7 @@ $bookings=[3,6,18,19,11,14];
                     echo   (floor($i/5)+1) . "排". ($i%5 +1 ) ."號";
                     echo "</div>";
                 if(!in_array($i,$bookings)){
-                    echo "<input type='checkbox' value='$i'>";
+                    echo "<input class='chk' type='checkbox' value='$i'>";
                 }
                 echo "</div>";
             }
@@ -80,3 +80,33 @@ $bookings=[3,6,18,19,11,14];
         <button>確定</button>
     </div>
 </div>
+
+
+<script>
+
+let seats=[];
+
+$(".chk").on("change",function(){
+
+    if($(this).prop('checked')){
+        //劃位
+        
+        if(seats.length>=4){
+            alert("最多只能購買四張票");
+            $(this).prop('checked',false)
+
+        }else{
+            seats.push($(this).val())
+        }
+
+    }else{
+        //取消劃位
+        seats.splice(seats.indexOf($(this).val()),1)
+    }
+    ///console.log(seats)
+
+})
+
+
+
+</script>
