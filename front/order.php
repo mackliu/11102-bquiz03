3ecfa0600c30d1bbeebdfc489667463b89c9c2ea
mackliu <mackliu@wda.gov.tw>
2023-02-10 13:31:48 +0800

    建立訂票選單及劃位畫面間的切換機制,使用js

--- front/order.php
+++ front/order.php
@@ -1,32 +1,48 @@
-<h3 class="ct">線上訂票</h3>
 
-<table style="width:50%;margin:auto">
-    <tr>
-        <td>電影:</td>
-        <td>
-            <select name="" id="movie"></select>
-        </td>
-    </tr>
-    <tr>
-        <td>日期:</td>
-        <td>
-            <select name="" id="day"></select>
-        </td>
-    </tr>
-    <tr>
-        <td>場次:</td>
-        <td>
-            <select name="" id="session"></select>
-        </td>
-    </tr>
-</table>
-<div class="ct">
-<button>確定</button>
-<button>重置</button>
+<div id="orderForm">
+
+    <h3 class="ct">線上訂票</h3>
+
+    <table style="width:50%;margin:auto">
+        <tr>
+            <td>電影:</td>
+            <td>
+                <select name="" id="movie"></select>
+            </td>
+        </tr>
+        <tr>
+            <td>日期:</td>
+            <td>
+                <select name="" id="day"></select>
+            </td>
+        </tr>
+        <tr>
+            <td>場次:</td>
+            <td>
+                <select name="" id="session"></select>
+            </td>
+        </tr>
+    </table>
+    <div class="ct">
+        <button onclick="$('#orderForm,#booking').toggle();getBooking()">確定</button>
+        <button>重置</button>
+    </div>
+
 </div>
+
+<div id="booking" style="display:none">
+
+
+<div class="ct">
+    <button onclick="$('#orderForm,#booking').toggle();$('#booking').html('')">上一步</button>
+    <button>確定</button>
+</div>
+</div>
+
+
+
+
 <script>
-
-
 
 getMovies();
 
@@ -48,9 +64,8 @@ function getMovies(){
         if(params.id){
             $(`#movie option[value="${params.id}"]`).attr("selected",true)
         }
-
+        
         getDays($("#movie").val());
-
     })
 }
 
