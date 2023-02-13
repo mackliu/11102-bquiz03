85aed45d1a773da5ee140bf91bc73d72f491ed9e
mackliu <mackliu@wda.gov.tw>
2023-02-13 13:17:26 +0800

    完成刪除單筆訂單資料功能

--- back/order.php
+++ back/order.php
@@ -24,9 +24,17 @@ foreach($orders as $order){
     <div style="width:14%;text-align:center"><?=$order['date'];?></div>
     <div style="width:14%;text-align:center"><?=$order['session'];?></div>
     <div style="width:14%;text-align:center"><?=$order['qt'];?></div>
-    <div style="width:14%;text-align:center"><?=$order['seats'];?></div>
     <div style="width:14%;text-align:center">
-        <button>刪除</button>
+    <?php
+        $seats=unserialize($order['seats']);
+        foreach($seats as $seat){
+            echo floor($seat/5)+1 . "排" . ($seat%5 +1 ) . "號";
+            echo "<br>";
+        }
+        ;?>
+    </div>
+    <div style="width:14%;text-align:center">
+        <button onclick="del('Order',<?=$order['id'];?>,'你確定要刪除<?=$order['date'].' '.$order['movie'].'的訂單資料嗎?';?>')">刪除</button>
     </div>
  </div>
  <hr>
